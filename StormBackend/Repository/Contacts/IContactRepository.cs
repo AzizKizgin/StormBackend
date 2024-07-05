61d267f1967141979d6ce7cf8090ee52c09b5fff
Aziz Kızgın <azizkzgn@gmail.com>
2024-07-05 11:38:07 +0300

    feat: IContactRepository functions changed

--- StormBackend/Repository/Contacts/IContactRepository.cs
+++ StormBackend/Repository/Contacts/IContactRepository.cs
@@ -9,9 +9,8 @@ namespace StormBackend.Repository.Contacts
 {
     public interface IContactRepository: IRepositoryBase<Contact>
     {
-        Task<Contact> GetContactAsync(int contactId, bool trackChanges);
-        Task<Contact> GetContactAsync(string contactUserName, bool trackChanges);
-        Task<List<Contact>> GetContactsAsync(GetContactsQuery query, bool trackChanges);
+        Task<Contact> GetContactAsync(string userId, string contactUserId, bool trackChanges);
+        Task<List<Contact>> GetContactsAsync(string userId, SearchContactsQuery query, bool trackChanges);
         void CreateContact(Contact contact);
         void UpdateContact(Contact contact);
         void DeleteContact(Contact contact);        
