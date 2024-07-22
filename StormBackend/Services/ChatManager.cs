761448180a33b7993d73854aef3d2a83749bd85a
Aziz Kızgın <azizkzgn@gmail.com>
2024-07-22 23:21:53 +0300

    feat: methods updated

--- StormBackend/Services/ChatManager.cs
+++ StormBackend/Services/ChatManager.cs
@@ -45,7 +45,7 @@ namespace StormBackend.Services
             await _manager.SaveAsync();
         }
 
-        public async Task DeleteMessage(string userId, int messageId)
+        public async Task<MessageDto> DeleteMessage(string userId, int messageId)
         {
             var message = await _manager.Message.GetMessageAsync(messageId, userId, false);
             if (message == null)
@@ -54,9 +54,12 @@ namespace StormBackend.Services
             }
             _manager.Message.DeleteMessage(message);
             await _manager.SaveAsync();
+            var messageDto = _mapper.Map<MessageDto>(message);
+            messageDto.Type = MessageType.Delete;
+            return messageDto;
         }
 
-        public async Task EditMessage(string userId, int messageId, string newContent)
+        public async Task<MessageDto> EditMessage(string userId, int messageId, string newContent)
         {
             var message = await _manager.Message.GetMessageAsync(messageId, userId, false);
             if (message == null)
@@ -64,8 +67,12 @@ namespace StormBackend.Services
                 throw new Exception("Message not found");
             }
             message.Content = newContent;
+            message.EditedAt = DateTime.Now;
             _manager.Message.UpdateMessage(message);
             await _manager.SaveAsync();
+            var messageDto = _mapper.Map<MessageDto>(message);
+            messageDto.Type = MessageType.Edit;
+            return messageDto;
         }
 
         public async Task<ChatDto> GetChat(string userId, string contactUserId)
@@ -106,7 +113,7 @@ namespace StormBackend.Services
             await _manager.SaveAsync();
         }
 
-        public async Task ReactToMessage(string userId, int messageId, string emoji)
+        public async Task<MessageDto> ReactToMessage(string userId, int messageId, string emoji)
         {
             var message = await _manager.Message.GetMessageAsync(messageId, userId, false);
             if (message == null)
@@ -119,13 +126,22 @@ namespace StormBackend.Services
                 UserId = userId,
                 Emoji = emoji
             };
+            var existingReaction = await _manager.EmojiReaction.GetEmoji(messageId, userId);
+            if (existingReaction != null)
+            {
+                _manager.EmojiReaction.DeleteEmoji(existingReaction);
+                message.Reactions.Remove(existingReaction);
+            }
             _manager.EmojiReaction.CreateEmoji(reaction);
             message.Reactions.Add(reaction);
             _manager.Message.UpdateMessage(message);
             await _manager.SaveAsync();
+            var messageDto = _mapper.Map<MessageDto>(message);
+            messageDto.Type = MessageType.Reaction;
+            return messageDto;
         }
 
-        public async Task ReadMessages(string userId, int chatId)
+        public async Task<List<MessageDto>> ReadMessages(string userId, int chatId)
         {
             var messages = await _manager.Message.GetMessagesAsync(chatId, false);
             foreach (var message in messages)
@@ -137,9 +153,15 @@ namespace StormBackend.Services
                 }
             }
             await _manager.SaveAsync();
+            var messageList = _mapper.Map<List<MessageDto>>(messages);
+            foreach (var message in messageList)
+            {
+                message.Type = MessageType.Read;
+            }
+            return messageList;
         }
 
-        public async Task SendMessage(string userId, string contactUserId, MessageDto message)
+        public async Task<MessageDto> SendMessage(string userId, string contactUserId, CreateMessageDto message)
         {
             var chat = await _manager.Chat.GetChatAsync(userId, contactUserId, false);
             if (chat == null)
@@ -166,20 +188,23 @@ namespace StormBackend.Services
                 ChatId = chat.Id,
                 Content = message.Content,
                 SenderId = userId,
-                Timestamp = DateTime.Now
+                CreatedAt = DateTime.Now
             };
             _manager.Message.CreateMessage(newMessage);
             await _manager.SaveAsync();
+            var messageDto = _mapper.Map<MessageDto>(newMessage);
+            messageDto.Type = MessageType.Add;
+            return messageDto;
         }
 
-        public async Task SendMessage(string userId, int chatId, MessageDto message)
+        public async Task<MessageDto> SendMessage(string userId, int chatId, CreateMessageDto message)
         {
             var newMessage = new Message
             {
                 ChatId = chatId,
                 Content = message.Content,
                 SenderId = userId,
-                Timestamp = DateTime.Now
+                CreatedAt = DateTime.Now
             };
             var chat = await _manager.Chat.GetChatByIdAsync(chatId, false);
             if (chat == null)
@@ -190,10 +215,12 @@ namespace StormBackend.Services
             _manager.Chat.UpdateChat(chat);
             _manager.Message.CreateMessage(newMessage);
             await _manager.SaveAsync();
-
+            var messageDto = _mapper.Map<MessageDto>(newMessage);
+            messageDto.Type = MessageType.Add;
+            return messageDto;
         }
 
-        public async Task UnreactToMessage(string userId, int messageId)
+        public async Task<MessageDto> UnreactToMessage(string userId, int messageId)
         {
             var reaction = await _manager.EmojiReaction.GetEmoji(messageId, userId);
             if (reaction == null)
@@ -202,6 +229,10 @@ namespace StormBackend.Services
             }
             _manager.EmojiReaction.DeleteEmoji(reaction);
             await _manager.SaveAsync();
+            var message = await _manager.Message.GetMessageAsync(messageId, userId, false);
+            var messageDto = _mapper.Map<MessageDto>(message);
+            messageDto.Type = MessageType.Reaction;
+            return messageDto;
         }
     }
 }
